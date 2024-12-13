fde5f97cd3465c73b31a26b17436721c282ef49d
nabilmersni <nabilmersni123@gmail.com>
2024-12-13 14:08:42 +0100

    complete the auth fonctionality in the backend

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,8 +1,8 @@
-import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 
 const pool = new Pool({
-  connectionString: "postgesql://postgres:test123@mydb:5432/mydb",
+  connectionString: "postgesql://postgres:test123@db:5432/mydb",
 });
 
 export const db = drizzle(pool);
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,8 +1,128 @@
-import { Router } from "express";
+import { NewUser } from "./../db/schema";
+import { eq } from "drizzle-orm";
+import { Request, Response, Router } from "express";
+import { db } from "../db";
+import { users } from "../db/schema";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { auth, AuthRequest } from "../middleware/auth";
+
 const authRouter = Router();
 
-authRouter.get("/", (req, res) => {
-  res.send("hey from auth route");
+interface SignUpBody {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface LoginBody {
+  email: string;
+  password: string;
+}
+
+authRouter.post(
+  "/signup",
+  async (req: Request<{}, {}, SignUpBody>, res: Response) => {
+    try {
+      const { name, email, password } = req.body;
+
+      const existUser = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, email));
+
+      if (existUser.length) {
+        res.status(400).json({
+          msg: "User with the same email already exist!",
+        });
+        return;
+      }
+      const hashedPassword = await bcryptjs.hash(password, 8);
+      const newUser: NewUser = {
+        name,
+        email,
+        password: hashedPassword,
+      };
+
+      const [addedUser] = await db.insert(users).values(newUser).returning();
+      res.status(201).json(addedUser);
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+);
+
+authRouter.post("/tokenIsValid", async (req, res) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) {
+      res.json(false);
+      return;
+    }
+    const verified = jwt.verify(token, "secretkey");
+
+    if (!verified) {
+      res.json(false);
+      return;
+    }
+
+    const verifiedToken = verified as { id: string };
+    const [user] = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, verifiedToken.id));
+
+    if (!user) {
+      res.json(false);
+      return;
+    }
+
+    res.json(true);
+  } catch (error) {
+    res.status(500).json(false);
+  }
+});
+
+authRouter.post(
+  "/login",
+  async (req: Request<{}, {}, LoginBody>, res: Response) => {
+    try {
+      const { email, password } = req.body;
+
+      const [existUser] = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, email));
+
+      if (!existUser) {
+        res.status(400).json({
+          msg: "Invalid credentials!",
+        });
+        return;
+      }
+      const isMatchPassword = await bcryptjs.compare(
+        password,
+        existUser.password
+      );
+
+      if (!isMatchPassword) {
+        res.status(400).json({
+          msg: "Invalid credentials!",
+        });
+        return;
+      }
+
+      const token = jwt.sign({ id: existUser.id }, "secretkey");
+
+      res.status(200).json({ token, ...existUser });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+);
+
+authRouter.get("/", auth, async (req: AuthRequest, res) => {
+  res.send(req.token);
 });
 
 export default authRouter;
